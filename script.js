895dc57863bf2ff875adb5711364f6200acc5155
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-18 00:49:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ function addList() {
 	var title = document.getElementById("title");
 	var author = document.getElementById("author");
 	var isbn = document.getElementById("isbn");
+
+	var deleteButton = document.createElement("button");
+	deleteButton.innerHTML = "X";
+	deleteButton.className = "delete";
+	deleteButton.style.backgroundColor = "red";
+	deleteButton.style.border = "none";
+	deleteButton.style.color = "white";
+	
 	
 	var row = table.insertRow(-1);
 	row.insertCell(0).innerHTML = title.value;
 	row.insertCell(1).innerHTML = author.value;
 	row.insertCell(2).innerHTML = isbn.value;
+	row.insertCell(3).appendChild(deleteButton);
+
+	deleteButton.addEventListener('click', function(e) {
+    var row = e.target.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+});
 
 	title.value = " ";
 	author.value = " ";
