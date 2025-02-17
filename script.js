9addb6c53a5564eea0ec4fedafa4c43e601b1fa4
Heer Gosrani <106440873+heergosrani02@users.noreply.github.com>
2025-02-18 00:20:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function addList() {
+	var table = document.getElementById("book-list");
+	var title = document.getElementById("title");
+	var author = document.getElementById("author");
+	var isbn = document.getElementById("isbn");
+	
+	var row = table.insertRow(-1);
+	row.insertCell(0).innerHTML = title.value;
+	row.insertCell(1).innerHTML = author.value;
+	row.insertCell(2).innerHTML = isbn.value;
+
+	title.value = " ";
+	author.value = " ";
+	isbn.value = " ";
+}
